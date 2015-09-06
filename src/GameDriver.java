24414a3a8ebfa9d57a438cf184f5f53b03048cfd
GoPro16 <kgray1497@gmail.com>
2015-09-06 00:36:29 -0400

    Up to Date Driver

--- src/GameDriver.java
+++ src/GameDriver.java
@@ -7,9 +7,9 @@ public class GameDriver
 	public static void main(String[] args)
 	{
 		//Getting the player name as input
-		CharTraits p1 = new CharTraits();
+		Player p1 = new Player();
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Input your name: ");
+		System.out.print("Input your name: ");
 		p1.setName(sc.next());
 		//Displaying player name
 		System.out.println("Player Name: " + p1.getName());
