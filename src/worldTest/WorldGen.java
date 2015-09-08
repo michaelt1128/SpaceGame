28d1575c49ee94bf5b8a78189504c8710c9f4a03
michaelt1128 <michael.turner1128@gmail.com>
2015-09-08 11:37:05 -0400

    ADDED THE ABILITY TO CHANGE TILES
    but broke gameOutput class.
    will fix i promise.
    maybe.

--- src/worldTest/WorldGen.java
+++ src/worldTest/WorldGen.java
@@ -12,77 +12,64 @@ public class WorldGen
  
 	JFrame frame=new JFrame("Universe"); //creates frame
     JButton[][] grid; //names the grid of buttons
-    JPanel ButtonPane = new JPanel();
-
+        
+    public static void main(String[] args)
+    {    	
+    	//create the icons from the image files
+    	Icon Kappa = null;
+    	Icon BibleThump = null;
+    	try
+    	{
+        	//Kappa = kappa image icon
+    		BufferedImage kappa = null;
+    		kappa = ImageIO.read(WorldGen.class.getResource("/images/Kappa.jpg"));
+    		Kappa = new ImageIcon(kappa);
+    		
+    		//BibleThump = BibleThump image icon
+    		BufferedImage bible = null;
+        	bible = ImageIO.read(WorldGen.class.getResource("/images/BibleThump.png"));
+    		BibleThump = new ImageIcon(bible);
+    	}
+    	catch(IOException e)
+    	{
+    		e.printStackTrace();
+    	}
+    	WorldGen universe = new WorldGen(10, 10, Kappa);
+    	universe.setTile(2, 2, BibleThump);
+    	universe.setTile(4, 3, BibleThump);
+    	universe.setTile(7, 8, BibleThump);
+    	universe.setTile(7, 9, BibleThump);
+    }
     
-    public WorldGen(int width, int length) //constructor
+    //the worldgen constructor creates a grid of buttons 
+    //the size and icon can be changed
+    public WorldGen(int width, int length, Icon image) 
     { 
-    	frame.setLayout(new GridLayout(width, length)); //set layout
+    	frame.setLayout(new GridLayout(width, length));
     	frame.setBackground(Color.BLACK);
     	frame.setSize(new Dimension(600, 600));
-        grid=new JButton[width][length]; //allocate the size of grid
+        grid=new JButton[width][length]; //allocate the size of button grid
         
         for(int y=0; y<length; y++)
         {
          	for(int x=0; x<width; x++)
           	{
-          		grid[x][y]=new JButton(""); //creates new button
-          		//grid[x][y].setPreferredSize(new Dimension(40,40));
-          		grid[x][y].setBackground(Color.blue);
-
-          		try
-            	{
-            		BufferedImage image = null;
-            		image = ImageIO.read(WorldGen.class.getResource("/images/Kappa.jpg"));    	
-            		grid[x][y].setIcon(new ImageIcon(image));
-            		
-            		frame.setVisible(true);
-            	}
-            	catch(IOException e)
-            	{
-            		e.printStackTrace();
-            	}
+          		grid[x][y]=new JButton("");
+          		grid[x][y].setBackground(Color.black);          		
+          		grid[x][y].setIcon(image);
           		grid[x][y].setSize(40, 40);
           		frame.add(grid[x][y]); //adds button to grid
             }
         }
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //frame.pack(); //sets appropriate size for frame
         frame.setVisible(true); //makes frame visible
         frame.setResizable(false);
     }
     
     //The goal of the setTile method is to replace tiles with other icons
-    public void setTile(int x_val, int y_val)
+    public void setTile(int x_val, int y_val, Icon image)
     {
-    	SwingWorker<Boolean, Void> worker = new SwingWorker<Boolean, Void>(){
-    		@Override
-    		protected Boolean doInBackground() throws Exception
-    		{
-    			return false;
-    		}
-    		
-    		@Override
-    		protected void done()
-    		{
-    			try {
-					Boolean status = get();
-					BufferedImage bible = null;
-            		bible = ImageIO.read(WorldGen.class.getResource("/images/BibleThump.png"));    	
-            		grid[x_val][y_val].setIcon(new ImageIcon(bible));
-            		
-				} catch (InterruptedException | ExecutionException | IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-    			
-    		}
-    	};
-    			
-
+    	grid[x_val][y_val].setIcon(image);
     }
-    /**public static void main(String[] args) 
-    {
-    	new WorldGen(10,10);//makes new WorldGen with 2 parameters
-    }**/
 }
+
