9d5c64f97d07ecd08eb04d38e9e78f644f0add52
GoPro16 <kgray1497@gmail.com>
2015-09-13 20:18:14 -0400

    Huge ass changes

--- src/gameTest/SpaceBattle.java
+++ src/gameTest/SpaceBattle.java
@@ -8,6 +8,8 @@ public class SpaceBattle
 {
 	public void startBattle(ArrayList<Integer> pStats,ArrayList<Integer> eStats)
 	{
+		JPanel panel1 = new JPanel();
+		panel1.setSize(500,250);
 		JFrame bFrame = new JFrame("Space Battle");
 		bFrame.setBackground(Color.BLACK);
 		bFrame.setLayout(new BorderLayout());
@@ -15,28 +17,33 @@ public class SpaceBattle
 		JLabel turnNum = new JLabel("Turn " + turnCount);
 		bFrame.add(turnNum, BorderLayout.PAGE_START);
 		turnNum.setFont(new Font("Serif", Font.BOLD, 24));
-		bFrame.setSize(350,350);
+		bFrame.setSize(800,400);
 		bFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		bFrame.setVisible(true);
 		JButton attackButton = new JButton("Attack");
-		attackButton.setSize(50,50);
+		attackButton.setSize(200,100);
 		attackButton.addActionListener(new ActionListener()
 				{
 				public void actionPerformed(ActionEvent e)
 				{
 					if(pStats.get(1) > 0 && eStats.get(1) > 0)
 					{
-						int eShield = eStats.get(0);
-						eShield = eStats.get(0) - pStats.get(2);
-						eStats.set(0,eShield);
-						JLabel bText = new JLabel("Enemy Shields are down to " + eStats.get(0) + "% Captain!");
-						bFrame.add(bText,BorderLayout.CENTER);
-						bText.setFont(new Font("Serif", Font.BOLD, 30));
+						if(eStats.get(0) > 0)
+						{
+							int tempE;
+							tempE = eStats.get(0) - pStats.get(2);
+							eStats.add(0,tempE);
+						}
+						else if(eStats.get(0) <= 0)
+						{
+							int tempE;
+							tempE = eStats.get(1) - pStats.get(2);
+							eStats.add(1,tempE);
+						}
 					}
 					
 				}
 				});
-		bFrame.add(attackButton, BorderLayout.AFTER_LAST_LINE);
 		JButton fleeButton = new JButton("Flee");
 		fleeButton.setSize(50,50);
 		fleeButton.addActionListener(new ActionListener()
@@ -44,11 +51,13 @@ public class SpaceBattle
 				public void actionPerformed(ActionEvent e)
 				{
 					JLabel fleeText = new JLabel("Lets get the Hell out of here!");
-					bFrame.add(fleeText,BorderLayout.CENTER);
+					bFrame.add(fleeText,BorderLayout.AFTER_LAST_LINE);
 					fleeText.setFont(new Font("Serif", Font.BOLD, 24));
 				}
 				});
-		bFrame.add(fleeButton, BorderLayout.AFTER_LAST_LINE);
+		panel1.add(attackButton);
+		panel1.add(fleeButton);
+		bFrame.add(panel1);
 		bFrame.setVisible(true);
 		
 	}
