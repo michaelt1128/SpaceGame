b3086773602245455fdf17d8c9d5972cadef0729
michaelt1128 <michael.turner1128@gmail.com>
2015-09-19 21:28:17 -0400

    added sun stuff

--- src/worldTest/GameUpdates.java
+++ src/worldTest/GameUpdates.java
@@ -1,6 +1,7 @@
 package worldTest;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -8,12 +9,13 @@ import javax.swing.JLabel;
 public class GameUpdates {
 	
 	JFrame frame = new JFrame("Space Game");
+
+	JLabel label = new JLabel("hi");
 	public GameUpdates()
 	{
 		frame.setLayout(new BorderLayout());		
 		frame.setSize(800, 300);	
-		
-		JLabel label = new JLabel("hi");
+		label.setFont(new Font("Serif", Font.BOLD, 24));
 		frame.add(label);
 		frame.setResizable(false);
 	}
