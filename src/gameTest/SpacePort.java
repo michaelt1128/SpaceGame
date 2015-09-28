74a62aaa8378d5c77cd28983c38d5788ec22980e
GoPro16 <kgray1497@gmail.com>
2015-09-28 11:42:25 -0400

    SpacePort Updates

--- src/gameTest/SpacePort.java
+++ src/gameTest/SpacePort.java
@@ -12,7 +12,7 @@ public class SpacePort
 	{
 		JFrame mFrame = new JFrame("Space Port");
 		mFrame.setLayout(new BorderLayout());
-		mFrame.setSize(800,400);
+		mFrame.setSize(800,200);
 		JPanel buttonPanel = new JPanel();
 		JPanel textPanel = new JPanel();
 		
@@ -27,17 +27,46 @@ public class SpacePort
 		{
 			public void actionPerformed(ActionEvent e)
 			{
+				int tempShields = pStats.get(1);
+				int maxShields = pStats.get(5);
 				int tempMoney = pStats.get(6);
 				int tempCost = (pStats.get(1) / pStats.get(5)) * 10000; //need a better formula to get cost of repairings shields
-				if(tempMoney > tempCost)
+				if(tempMoney > tempCost && tempShields != maxShields)
 				{
 					pStats.set(1, pStats.get(5));
 					pStats.set(6, tempMoney - tempCost);
+					repairsText.setText("Shields back at 100%");
 				}
-				else
+				else if(tempMoney > tempCost && tempShields == maxShields)
 				{
-					
+					repairsText.setText("Shields already at Maximum");
 				}
+				else if(tempMoney < tempCost)
+				{
+					repairsText.setText("Insufficent Credits");
+				}
+				repairsText.setVisible(true);
+			}
+		});
+		
+		buyArmor.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				int tempMoney = pStats.get(6);
+				int tempCost = 2000;
+				int armorBuff = (int) (Math.random() * 100);
+				if(tempMoney > tempCost)
+				{
+					pStats.set(6, pStats.get(6) - tempCost);
+					pStats.set(2, pStats.get(2) + armorBuff);
+					repairsText.setText("Armor Total: " + pStats.get(2));
+				}
+				else if(tempMoney < tempCost)
+				{
+					repairsText.setText("Insufficent Credits");
+				}
+				repairsText.setVisible(true);
 			}
 		});
 		
